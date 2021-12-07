e9a738006684beeba6e0932912ad570c4f86e7c1
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2021-12-07 16:37:20 +0100

    Various cleanups
     - the key material is now inserted via secrets.  That means that we
       don't have to rebuild the container every time, and it also means
       that anyone with access to the cluster can now deploy instances.
    
     - split out new `teardown` script to shut down running instances
    
     - `all` is now called `deploy`
    
     - reduce duplication of hostnames and such

--- container/run.sh
+++ container/run.sh
@@ -5,7 +5,7 @@ export G_MESSAGES_DEBUG=all
 XDG_CONFIG_DIRS=/container/etc /usr/libexec/cockpit-ws --for-tls-proxy -p 9090 &
 
 /container/cockpit-cloud-connector \
-    --key /container/server.key \
-    --cert /container/server.crt \
-    --peer-cert /container/client.crt \
+    --key /container/secrets/server.key \
+    --cert /container/secrets/server.crt \
+    --peer-cert /container/secrets/client.crt \
     server -p 8080 /tmp/socket
